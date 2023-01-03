8efbb5e3202e6bd4a640bc396d50e99a183bad63
shedonyii <shedonyii@gmail.com>
2023-01-03 12:24:50 -0800

    shedonyii

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
-for i in range(ord('a'), ord('z') + 1):
-    print('{:c}'.format(i), end='')
+
+for character in range(97, 123):
+
+        print("{:c}".format(character), end='')
+
+
